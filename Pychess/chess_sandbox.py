56d28e79376ba75dbaf94fcdc8220d057a5aad6c
kalchaar <keenan.alchaar@gmail.com>
2022-09-22 03:09:59 -0400

    chess script ends on checkmate; can pass in a FEN to place the board in a particular starting state for easy debugging;

--- Pychess/chess_sandbox.py
+++ Pychess/chess_sandbox.py
@@ -1,27 +1,50 @@
 import chess
+import sys
+import os
 from stockfish import Stockfish
 from time import sleep
 
 IN_PROGRESS = True
 TERMINATED = False
+global game_state
 game_state = IN_PROGRESS
 
 def main():
-    stockfish = Stockfish(path="C:/Users/sirmu/Documents/Keenan/The_UVA_Archives/Capstone/stockfish_15_win_x64_avx2/stockfish_15_x64_avx2.exe")
-    stockfish.update_engine_parameters({'Hash':256})
-    stockfish.set_elo_rating(2500)
+    stockfish = Stockfish(path="/home/thegreatgambit/Documents/Capstone-Software/Pychess/Capstone-Stockfish/src/stockfish")
+    stockfish.update_engine_parameters({'Hash':64})
+    stockfish.set_elo_rating(2000)
+    if len(sys.argv) > 1:
+        if stockfish.is_fen_valid(sys.argv[1]):
+            stockfish.set_fen_position(sys.argv[1])
+        else:
+            print("Invalid FEN; exiting...")
+            return None
     player_color = input("Select a color (\"W\" or \"B\"): ")
     while player_color not in ["W", "B"]:
         player_color = input("Incorrect input. Select a color (\"W\" or \"B\"): ")
     if player_color == "W":
-        while game_state == IN_PROGRESS:
-            player_move(stockfish, player_color)
-            check_game_state()
-            stockfish_move(stockfish, player_color)
+        while True:
+            p_move = player_move(stockfish, player_color)
+            check_game_state(p_move)
+            if game_state == TERMINATED:
+                break
+
+            sf_move = stockfish_move(stockfish, player_color)
+            check_game_state(sf_move)
+            if game_state == TERMINATED:
+                break
     else:
-        while game_state == IN_PROGRESS:
-            stockfish_move(stockfish, player_color)
-            player_move(stockfish, player_color)
+        while True:
+            sf_move = stockfish_move(stockfish, player_color)
+            check_game_state(sf_move)
+            if game_state == TERMINATED:
+                break
+
+            p_move = player_move(stockfish, player_color)
+            check_game_state(p_move)
+            if game_state == TERMINATED:
+                break
+
     print("Thanks for playing!")
 
 def player_move(stockfish:Stockfish, player_color):
@@ -35,18 +58,18 @@ def player_move(stockfish:Stockfish, player_color):
             print("That move is invalid. Please try again.\n")
         else:
             valid_move = True
-            return stockfish.get_fen_position()
+            return chess.Board(stockfish.get_fen_position())
 
 
 
 def stockfish_move(stockfish:Stockfish, player_color):
     print(stockfish.get_board_visual(perspective_white=(True if player_color=="W" else False)))
     print("Stockfish making move...\n")
-    stockfish_next_move = stockfish.get_best_move_time(1000)
+    stockfish_next_move = stockfish.get_best_move_time(3000)
     stockfish.make_moves_from_current_position([stockfish_next_move])
     print(f"Stockfish's move: {stockfish_next_move}")
-    print(stockfish.get_evaluation())
-    return stockfish.get_fen_position()
+    #print(stockfish.get_evaluation())
+    return chess.Board(stockfish.get_fen_position())
 
 def check_game_state(board:chess.Board):
     if board.is_stalemate():
